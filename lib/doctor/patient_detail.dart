a6ad8cba925575ee7a9e4034ecfdb69a97e0c715
Boujyass <boujelbene.yassine@enis.tn>
2024-04-02 22:19:32 +0100

    null bug

--- lib/doctor/patient_detail.dart
+++ lib/doctor/patient_detail.dart
@@ -1,11 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PatientProfileScreen extends StatefulWidget {
   final String patientId;
 
-  const PatientProfileScreen({Key? key, required this.patientId})
-      : super(key: key);
+  const PatientProfileScreen({Key? key, required this.patientId}) : super(key: key);
 
   @override
   _PatientProfileScreenState createState() => _PatientProfileScreenState();
@@ -40,7 +39,8 @@ class _PatientProfileScreenState extends State<PatientProfileScreen> {
     }
   }
 
-  String getGenderString(String gender) {
+  String getGenderString(String? gender) {
+    if (gender == null) return 'N/A';
     switch (gender) {
       case 'male':
         return 'Male';
@@ -51,15 +51,18 @@ class _PatientProfileScreenState extends State<PatientProfileScreen> {
     }
   }
 
-  String getHasChromaticDiseaseString(bool hasChromaticDisease) {
+  String getHasChromaticDiseaseString(bool? hasChromaticDisease) {
+    if (hasChromaticDisease == null) return 'N/A';
     return hasChromaticDisease ? 'Yes' : 'No';
   }
 
-  String getIsTakingMedicineString(bool isTakingMedicine) {
+  String getIsTakingMedicineString(bool? isTakingMedicine) {
+    if (isTakingMedicine == null) return 'N/A';
     return isTakingMedicine ? 'Yes' : 'No';
   }
 
-  String getSuicidalThoughtsString(String suicidalThoughts) {
+  String getSuicidalThoughtsString(String? suicidalThoughts) {
+    if (suicidalThoughts == null) return 'N/A';
     switch (suicidalThoughts) {
       case 'نعم':
         return 'Patient has expressed suicidal thoughts';
@@ -86,126 +89,122 @@ class _PatientProfileScreenState extends State<PatientProfileScreen> {
         backgroundColor: Colors.indigo[800],
         title: const Text('Patient Profile'),
       ),
-      body: patientInfo.isNotEmpty && formData.isNotEmpty
-          ? SingleChildScrollView(
-              child: Container(
-                color: Colors.grey[100],
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 16),
-                      ListTile(
-                        title: Text(
-                          '${patientInfo['name']} ${patientInfo['family_name']}',
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.grey[100],
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 16),
+                ListTile(
+                  title: Text(
+                    '${patientInfo['name'] ?? 'N/A'} ${patientInfo['family_name'] ?? 'N/A'}',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.indigo[800],
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Patient ID: ${widget.patientId}',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  trailing: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(patientInfo['avatarUrl'] ?? ''),
+                  ),
+                ),
+                SizedBox(height: 24),
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Personal Information',
                           style: TextStyle(
-                            fontSize: 24,
+                            fontSize: 18,
                             fontWeight: FontWeight.bold,
                             color: Colors.indigo[800],
                           ),
                         ),
-                        subtitle: Text(
-                          'Patient ID: ${widget.patientId}',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey[600],
-                          ),
+                        Divider(height: 20, thickness: 1),
+                        ListTile(
+                          title: Text('Age: ${patientInfo['age'] ?? 'N/A'}'),
+                          subtitle: Text('Gender: ${getGenderString(patientInfo['gender'])}'),
                         ),
-                        trailing: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: NetworkImage(patientInfo['avatarUrl']),
+                        ListTile(
+                          title: Text('Has Chromatic Disease: ${getHasChromaticDiseaseString(patientInfo['has_chromatic_disease'])}'),
+                          subtitle: Text('Taking Medicine: ${getIsTakingMedicineString(patientInfo['is_taking_medicine'])}'),
                         ),
-                      ),
-                      SizedBox(height: 24),
-                      Card(
-                        elevation: 4,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Personal Information',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.indigo[800],
-                                ),
-                              ),
-                              Divider(height: 20, thickness: 1),
-                              ListTile(
-                                title: Text('Age: ${patientInfo['age']}'),
-                                subtitle: Text('Gender: ${getGenderString(patientInfo['gender'])}'),
-                              ),
-                              ListTile(
-                                title: Text('Has Chromatic Disease: ${getHasChromaticDiseaseString(patientInfo['has_chromatic_disease'])}'),
-                                subtitle: Text('Taking Medicine: ${getIsTakingMedicineString(patientInfo['is_taking_medicine'])}'),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 24),
-                      Card(
-                        elevation: 4,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'HAD Form Interpretation',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.indigo[800],
-                                ),
-                              ),
-                              Divider(height: 20, thickness: 1),
-                              ListTile(
-                                title: Text('Anxiety Interpretation: ${formData['anxietyInterpretation']}'),
-                                subtitle: Text('Depression Interpretation: ${formData['depressionInterpretation']}'),
-                              ),
-                              ListTile(
-                                title: Text(getSuicidalThoughtsString(formData['suicidalThoughts'])),
-                                subtitle: Text('Last Form Submission: ${formData['timestamp'].toDate().toString().split(' ').first}'),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 24),
-                      Center(
-                        child: ElevatedButton(
-                          onPressed: navigateToFormHistory,
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.indigo[800],
-                            padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(24),
-                            ),
-                          ),
-                          child: Text(
-                            'View Form History',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            )
-          : Center(
-              child: CircularProgressIndicator(),
+                SizedBox(height: 24),
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'HAD Form Interpretation',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.indigo[800],
+                          ),
+                        ),
+                        Divider(height: 20, thickness: 1),
+                        ListTile(
+                          title: Text('Anxiety Interpretation: ${formData['anxietyInterpretation'] ?? 'N/A'}'),
+                          subtitle: Text('Depression Interpretation: ${formData['depressionInterpretation'] ?? 'N/A'}'),
+                        ),
+                        ListTile(
+                          title: Text(getSuicidalThoughtsString(formData['suicidalThoughts'])),
+                          subtitle: Text('Last Form Submission: ${formData['timestamp']?.toDate().toString().split(' ').first ?? 'N/A'}'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 24),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: navigateToFormHistory,
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.indigo[800],
+                      padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                    child: Text(
+                      'View Form History',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ),
+              ],
             ),
+          ),
+        ),
+      ),
     );
   }
 }
